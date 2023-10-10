cb779f49471f31ca4f6cbdcad42108e22eb6f726
israt-emu <isratjahan.emu1998@gmail.com>
2023-10-10 22:05:03 +0600

    category controller and route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import {AuthRoutes} from "../modules/auth/auth.route";
+import {UserRoutes} from "../modules/user/user.route";
 
 const router = express.Router();
 //
-const moduleRoutes = [{path: "/auth", route: AuthRoutes}];
+const moduleRoutes = [
+  {path: "/auth", route: AuthRoutes},
+  {path: "/users", route: UserRoutes},
+  {path: "/categories", route: AuthRoutes},
+  {path: "/books", route: AuthRoutes},
+  {path: "/orders", route: AuthRoutes},
+];
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
